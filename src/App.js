4effe6d69f5eb6f69950b9e0d6e86cd7e7acec95
alyque12 <alyque@LAPTOP-DQ0BLN67>
2023-03-11 14:52:22 -0500

    Added link and made buttons dynamic props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import TopicSelector from './components/TopicSelector';
 
 function App() {
   return (
     <div className="App">
-      <TopicSelector/>
     </div>
   );
 }
