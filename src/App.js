2871678b3e31e4a7ea950e4702e7ce2a208c567a
John Khachian <jaksawesome10@gmail.com>
2023-03-11 16:03:19 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Landing from "./screens/Landing";
+import Home from "./screens/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Landing</Link>
-        <Link to="/home">Home</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
