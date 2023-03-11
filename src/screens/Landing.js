cf9687d01c815af632ea0bb282f707b0f71d5e2e
John Khachian <jaksawesome10@gmail.com>
2023-03-11 14:55:04 -0500

    landing screen

---
+++ src/screens/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/screens/Landing.css";
+
+const Landing = () => {
+    return (
+        <div className="container">
+            <img src="./logo.svg"></img>
+        </div>
+    );
+};
+
+export default Landing;
