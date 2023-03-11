095e267a2e377c5b0809d49efcf3a311ff8b2c2d
alyque12 <alyque@LAPTOP-DQ0BLN67>
2023-03-11 13:14:31 -0500

    Added Primary and Secondary Buttons

---
+++ src/components/ButtonPrimary.js
@@ -0,0 +1,9 @@
+import '../styles/components/ButtonPrimary-style.css';
+
+const PrimaryButton = () => {
+    return(
+        <button className='Button'>Button</button>
+    )
+}
+
+export default PrimaryButton;
---
+++ src/components/ButtonSecondary.js
@@ -0,0 +1,9 @@
+import '../styles/components/ButtonSecondary-style.css'
+
+const ButtonSecondary = () => {
+    return (
+        <button className="Button">Button</button>
+    )
+}
+
+export default ButtonSecondary
