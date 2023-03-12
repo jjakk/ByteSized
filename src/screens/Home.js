cb04e6d4b8f5279c6fec626482733141f154db39
John Khachian <jaksawesome10@gmail.com>
2023-03-12 11:14:23 -0400

    fixes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
 
     return(
         <div className="container">
-            <img className='logo 'src='./logo.svg'></img>
+            <img className='logo' src='./logo.svg'></img>
             <Header style={{alignSelf: "center"}}>Weekly Bytes</Header>
             <div className="topic-list">
                 {topicList}
